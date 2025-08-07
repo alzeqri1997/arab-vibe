75ed02ccb931abcf856906c415a682fcee278fac
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-08-07 17:39:53 +0300

    setup background job with inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import { caller } from "@/trpc/server";
+"use client"
 
-export default async function Home() {
-  const data = await caller.createAPI({ text: 'Ahmed From Server Component'})
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+export default function Home() {
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => { 
+      alert("Function invoked successfully!")
+    }
+  }))
   return (
     <div>
-      {JSON.stringify(data)}
+      <button className="bg-white text-black rounded-lg px-4 py-2 mt-4" onClick={() => invoke.mutate({ text: "Ahmed" })}>
+        Invoke Function
+      </button>
     </div>
   );
 }
