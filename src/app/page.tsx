d62e7467bd3484acb43ceb0c42e708e38945704f
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-08-10 16:28:25 +0300

    setup inngest kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState<string>("")
   const trpc = useTRPC()
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => { 
@@ -12,7 +14,13 @@ export default function Home() {
   }))
   return (
     <div>
-      <button className="bg-white text-black rounded-lg px-4 py-2 mt-4" onClick={() => invoke.mutate({ text: "Ahmed" })}>
+      <input
+        type="text"
+        placeholder="Enter text"
+        className="border rounded-lg px-4 py-2"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="bg-white text-black rounded-lg px-4 py-2 mt-4" onClick={() => invoke.mutate({ text: value })}>
         Invoke Function
       </button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
-import { inngest } from "./client";
+/** @format */
+
+import { createAgent, openai } from "@inngest/agent-kit"
+import { inngest } from "./client"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
-);
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer. you summarize in 2 words.",
+      model: openai({ model: "gpt-4o" }),
+    })
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.email}`
+    )
+
+    return { output }
+  }
+)
