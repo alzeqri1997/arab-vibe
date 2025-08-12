854c2269053093e1c9b13d12bc4bd17527f4f402
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-08-12 16:45:29 +0300

    setup E2B

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,38 @@
-/** @format */
-
+// run this command in the terminal to open up inngest cli : npx inngest-cli@latest dev
 import { createAgent, openai } from "@inngest/agent-kit"
+import Sandbox from "@e2b/code-interpreter"
+
 import { inngest } from "./client"
+import { getSandbox } from "./utils"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
-    const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer. you summarize in 2 words.",
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandboxId = await Sandbox.create("yjwnm2zu3bj6qbkbck3z")
+
+      return sandboxId.sandboxId
+    })
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
       model: openai({ model: "gpt-4o" }),
     })
-    const { output } = await summarizer.run(
-      `Summarize the following text: ${event.data.email}`
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.email}`
     )
 
-    return { output }
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId)
+      const host = sandbox.getHost(3000)
+
+      return `https://${host}`
+    })
+
+    return { output, sandboxUrl }
   }
 )
