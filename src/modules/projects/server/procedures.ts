771c444f927a20da8d4f1ae9e1301c6b2d0a71d5
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-08-24 17:17:59 +0300

    add projects procedures

---
+++ src/modules/projects/server/procedures.ts
@@ -0,0 +1,52 @@
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { generateSlug } from "random-word-slugs";
+
+import { inngest } from "@/inngest/client";
+import prisma from "@/lib/prisma";
+
+export const projectsRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const projects = await prisma.project.findMany({
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+
+    return projects;
+  }),
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z
+          .string()
+          .min(1, { message: "Prompt is requited" })
+          .max(1000, { message: "Prompt has reached the limit" }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createdProject = await prisma.project.create({
+        data: {
+          name: generateSlug(2, {
+            format: "kebab",
+          }),
+          messages: {
+            create: {
+              content: input.value,
+              role: "USER",
+              type: "RESULT",
+            },
+          },
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+          projectId: createdProject.id
+        },
+      });
+      return createdProject;
+    }),
+});
