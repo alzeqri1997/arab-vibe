57a11b18f496d508a1b9be2f6a7450a519325f5d
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-09-16 18:05:45 +0300

    create fragment view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -9,14 +9,14 @@ import {
 } from "@/components/ui/resizable";
 import MessagesContainer from "../components/messages-container";
 import ProjectHeader from "../components/project-header";
+import FragmentWeb from "../components/fragment-web";
 
 interface Props {
   projectId: string;
 }
 
 const ProjectView = ({ projectId }: Props) => {
-
-  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null)
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
 
   return (
     <div className="h-screen">
@@ -26,19 +26,20 @@ const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
-          <Suspense fallback={<p>loading project </p>} >
+          <Suspense fallback={<p>loading project </p>}>
             <ProjectHeader projectId={projectId} />
           </Suspense>
-          <Suspense fallback={<p>loading messages only</p>} >
+          <Suspense fallback={<p>loading messages only</p>}>
             <MessagesContainer
               activeFragment={activeFragment}
               setActiveFragment={setActiveFragment}
-              projectId={projectId} />
+              projectId={projectId}
+            />
           </Suspense>
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={65} minSize={50}>
-          TODO:: Create Project
+          {!!activeFragment && <FragmentWeb data={activeFragment} />}
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
