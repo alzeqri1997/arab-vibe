ae4c5b63e4a224079a67cd257a0a5a345df2727b
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-09-16 18:05:19 +0300

    create hint component

---
+++ src/components/hint.tsx
@@ -0,0 +1,28 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+  children: React.ReactNode;
+  text: string;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+const Hint = ({ children, text, side, align }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default Hint;
