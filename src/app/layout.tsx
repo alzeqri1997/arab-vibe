2acfc10be4fa02a525ea1f1f8471e40624189029
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-09-25 12:01:08 +0300

    hide hydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <TRPCReactProvider>
-      <html lang="en" suppressContentEditableWarning>
+      <html lang="en" suppressHydrationWarning>
         {/* <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         > */}
