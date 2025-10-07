e078145e8a4d5eba0da7775d010d2020f4dda6b4
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-10-07 20:50:20 +0300

    setup i18n

--- src/app/(lang)/[lang]/(home)/page.tsx
+++ src/app/(lang)/[lang]/(home)/page.tsx
@@ -1,8 +1,10 @@
 import ProjectForm from "@/modules/home/ui/components/project-form";
 import ProjectsList from "@/modules/home/ui/components/projects-list";
 import Image from "next/image";
+import { getTranslations } from "next-intl/server";
 
-export default function Home() {
+export default async function Home() {
+  const t = await getTranslations("home")
   return (
     <div className="flex flex-col max-w-5xl mx-auto w-full">
       <section className="space-y-6 py-[16vh] 2xl:py-48">
@@ -16,10 +18,10 @@ export default function Home() {
           />
         </div>
         <h1 className="text-2xl md:text-5xl font-bold text-center">
-          Build something with Arab Vibe
+          {t('heading')}
         </h1>
         <p className="text-lg md:text-muted-foreground text-center">
-          Create apps and websites by chatting with AI
+          {t('sub-heading')}
         </p>
         <div className="max-w-3xl mx-auto w-full">
           <ProjectForm />
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,38 @@
+import createMiddleware from "next-intl/middleware";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+import { routing } from "./i18n/routing";
+
+const handleI18nRouting = createMiddleware(routing);
 const isPublicRoute = createRouteMatcher([
+  "",
   "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
+  "/:lang",
+  "/:lang/",
+  "/:lang/sign-in(.*)",
+  "/:lang/sign-up(.*)",
+  "/:lang/api(.*)",
   "/api(.*)",
-  "/pricing(.*)"
+  "/:lang/pricing(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
+  if (!isPublicRoute(req)) await auth.protect();
+
+  const { pathname } = req.nextUrl;
+
+  // Don't redirect api routes
+  if (pathname.startsWith('/api')) return
+
+  return handleI18nRouting(req);
 });
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    "/:locale/(api|trpc)(.*)",
   ],
+  // matcher: ['/', '/(ar|en)/:path*']
 };
