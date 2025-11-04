c04947224981eff7ca18e70a5326a14201eaf823
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2025-11-04 21:21:36 +0300

    add custom cookie

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,4 +3,10 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["ar", "en"],
   defaultLocale: "ar",
+  localeCookie: {
+    // Custom cookie name
+    name: 'USER_LOCALE',
+    // Expire in one year
+    maxAge: 60 * 60 * 24 * 365
+  }
 });
